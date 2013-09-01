96df9d998fa039469ea06b191b05b2304c8bac17
Cheng-Yu Lin <ntuaha@gmail.com>
2013-09-01 22:12:36 +0800

    去除不必要的資訊

--- src/extract/download.py
+++ src/extract/download.py
@@ -8,21 +8,6 @@ import numpy as np
 import webbrowser
 import os
 
-#下載2-5使用
-def download(year,month):
-	password = "aruxopq0rliabmUVxillmnWcop79abcdLXpemqeZijklqefgelfqeiIJKprlineYdeaBNyneytthjjlobc93A456hijki"
-	random_number = np.random.random_integers(0,len(password)-1,8)
-	pwd  = "".join([ password[i] for i in random_number])
-	#server_path = "https://survey.banking.gov.tw/statis/stmain.jsp?sys=120&ym=%d%02d&ymt=%d%02d&kind=0&type=2&funid=2050&cycle=0&outmode=3&rdm=%8s" %(year,month,year,month,pwd)
-	server_path = "https://survey.banking.gov.tw/statis/2_1_%d%02d%d%02d/22050.xls" % (year,month,year,month)
-	client_path = "/Users/aha/Dropbox/Project/Financial/Data/%d%02d.xls" % (year,month)
-	cookie_path = '/Users/aha/Dropbox/Project/Financial/Data/cookie0001.txt'
-	agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31'
-	print server_path
-	print client_path
-	cmd = 'curl -A "%s" -o %s -D %s %s'% (agent,client_path,cookie_path,server_path)
-	os.system(cmd)
-
 def getPassword():
 	password = "aruxopq0rliabmUVxillmnWcop79abcdLXpemqeZijklqefgelfqeiIJKprlineYdeaBNyneytthjjlobc93A456hijki"
 	random_number = np.random.random_integers(0,len(password)-1,8)
@@ -35,8 +20,6 @@ def checkFolder(folder):
 
 
 def download_2(base_path,year,month,categories_idx,filename,foldername):
-	#pwd = getPassword()
-	#server_path = "https://survey.banking.gov.tw/statis/stmain.jsp?sys=120&ym=%d%02d&ymt=%d%02d&kind=0&type=2&funid=2050&cycle=0&outmode=3&rdm=%8s" %(year,month,year,month,pwd)                    
 	server_path = "https://survey.banking.gov.tw/statis/%s_%d%02d%d%02d/%s.xls" % (categories_idx,year,month,year,month,filename)
 	client_path = "%srawdata/%s/%d%02d.xls" % (base_path,foldername,year,month)
 	cookie_path = "%scookie0001.txt" % (base_path)
