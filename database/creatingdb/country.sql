fc114d2ac19f8c68b87ecbdda4f4311685689a4a
Cheng-Yu Lin <ntuaha@gmail.com>
2013-09-22 14:39:03 +0800

    增加Country的資料表程式 #33

---
+++ database/creatingdb/country.sql
@@ -0,0 +1,22 @@
+create table `COUNTRY`(
+Data_Ym timestamp primary key,
+M1B real,
+F_Ins_Dp real,
+F_Ins_Ln real,
+C_Ins_Dp real,
+C_INs_Ln real,
+Country_SME real,
+Country_C_CL real,
+Country_CL real,
+MF_Bal real,
+Bund_Txn_Amt real,
+Stock_Index real,
+WPI real,
+CPI real,
+Exchange_Rate real,
+Rediscount_Rate real,
+B_Ins_Rate real,
+GDP real,
+Y_DP real
+);
+
